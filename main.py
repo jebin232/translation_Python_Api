01fb1fa3e058bf764033fce3dee718a11beed850
Jebin_M <90751436+jebin232@users.noreply.github.com>
2024-05-04 21:13:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def translate_text():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='0.0.0.0', port=8000)
