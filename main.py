1afd3182f7ad80138389c0108690a0ebcb777cbe
Jebin_M <90751436+jebin232@users.noreply.github.com>
2024-05-11 19:53:06 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, jsonify
+import traceback
 from googletrans import Translator
 import json
 
 app = Flask(__name__)
 translator = Translator()
 
-@app.route('/translate', methods=['POST'])
+@app.route('/tamil', methods=['POST'])
 def translate_text():
     try:
         data = request.get_json()
@@ -23,7 +24,72 @@ def translate_text():
         return json.dumps({'translated_text': translated_text}, ensure_ascii=False), 200
 
     except Exception as e:
+        traceback.print_exc()
         return jsonify({'error': 'An unexpected error occurred.'}), 500
+
+@app.route('/japan', methods=['POST'])
+def translate_text():
+    try:
+        data = request.get_json()
+        text = data.get('text')
+
+        if not isinstance(text, str):
+            return jsonify({'error': 'Invalid input. Text should be a string.'}), 400
+
+        if not text:
+            return jsonify({'error': 'Text to translate is missing'}), 400
+
+        translation = translator.translate(text, src='en', dest='ja')
+        translated_text = translation.text if translation else "Translation failed"
+        # Convert to JSON string with Unicode characters
+        return json.dumps({'translated_text': translated_text}, ensure_ascii=False), 200
+
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': 'An unexpected error occurred.'}), 500
+
+@app.route('/german', methods=['POST'])
+def translate_text():
+    try:
+        data = request.get_json()
+        text = data.get('text')
+
+        if not isinstance(text, str):
+            return jsonify({'error': 'Invalid input. Text should be a string.'}), 400
+
+        if not text:
+            return jsonify({'error': 'Text to translate is missing'}), 400
+
+        translation = translator.translate(text, src='en', dest='de')
+        translated_text = translation.text if translation else "Translation failed"
+        # Convert to JSON string with Unicode characters
+        return json.dumps({'translated_text': translated_text}, ensure_ascii=False), 200
+
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': 'An unexpected error occurred.'}), 500
+
+@app.route('/hindi', methods=['POST'])
+def translate_text():
+    try:
+        data = request.get_json()
+        text = data.get('text')
+
+        if not isinstance(text, str):
+            return jsonify({'error': 'Invalid input. Text should be a string.'}), 400
+
+        if not text:
+            return jsonify({'error': 'Text to translate is missing'}), 400
+
+        translation = translator.translate(text, src='en', dest='hi')
+        translated_text = translation.text if translation else "Translation failed"
+        # Convert to JSON string with Unicode characters
+        return json.dumps({'translated_text': translated_text}, ensure_ascii=False), 200
+
+    except Exception as e:
+        traceback.print_exc()
+        return jsonify({'error': 'An unexpected error occurred.'}), 500
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
